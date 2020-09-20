3f1ea21d8347bd84251beaf40a852b878ca3f6c1
Yhoundeh <aarond96@byui.edu>
2020-09-19 21:48:32 -0600

    Working (?) javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+function year() {
+    var d = new Date();
+    document.write(d.getFullYear()); 
+  }
+  
+  function modified() {
+    var m = new Date();
+    document.write(document.lastModified)
+  }
