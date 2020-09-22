920343e6a0dfe6175e86f3a29944c83263c11772
Yhoundeh <aarond96@byui.edu>
2020-09-21 21:23:31 -0600

    update javasript and minimize photo

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 function year() {
-    var d = new Date();
+    //var d = new Date();
     document.write(d.getFullYear()); 
   }
   
   function modified() {
-    var m = new Date();
+    //var m = new Date();
     document.write(document.lastModified)
   }
+
+
+
+
+
+ /* window.addEventListener('load',(event)=>) {
+    const lu = document.lastModified;
+
+    const cry = document.quarySelector("#copyrightyear");
+  }
+  */
