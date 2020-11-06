4a5d9a014a469411d99888c9977e6d2bee2273ce
Yhoundeh <aarond96@byui.edu>
2020-11-06 16:00:22 -0700

    Inital comit

--- dad-jokes/js/dad-jokes.js
+++ dad-jokes/js/dad-jokes.js
@@ -15,8 +15,18 @@ async function getDadJoke(){
 function newJoke(){
     const dad_id = document.getElementById("dad-joke");
     dad_id.innerHTML = "";
+    dad_id.classList.remove("small");
     const joke = getDadJoke()
     .then(function(j){
         dad_id.innerHTML = j['joke'];
+        if(j['joke'].length >85){
+            dad_id.classList.add("small");
+        }
     })
+    .catch(function(error){
+        dad_id.innerHTML = error;
+    });
 }
+window.addEventListener('load',(event)=>{
+    newJoke();
+})
