056b47fae6d13f41d7c583232aaea2944c3f3da5
Yhoundeh <aarond96@byui.edu>
2020-12-14 04:29:38 -0700

    weather forcast

---
+++ Lesson10/js/weatherapi.js
@@ -0,0 +1,14 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=7ad91157396706864277130b86e11838&units=imperial";
+
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+    const desc = jsObject.weather[0].description;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
